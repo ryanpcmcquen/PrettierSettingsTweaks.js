861137b294b7a3c7d320175a96fdd796b1d76565
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2018-01-22 21:14:12 -0800

    Always use parens in arrow functions.

--- PrettierSettingsTweaks.js
+++ PrettierSettingsTweaks.js
@@ -1,4 +1,5 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", () => {
     "use strict";
     document.getElementById("tabWidth").value = 4;
+    document.getElementById("arrowParens").value = "always";
 });
