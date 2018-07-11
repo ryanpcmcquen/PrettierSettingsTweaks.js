7b5e3c760d567693bcc52b75c7ce4de77dfaf639
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-07-11 15:26:27 -0700

    Fix selectors after website changes. Use single quotes.

--- PrettierSettingsTweaks.js
+++ PrettierSettingsTweaks.js
@@ -1,5 +1,13 @@
-window.addEventListener("load", () => {
-    "use strict";
-    document.getElementById("tabWidth").value = 4;
-    document.getElementById("arrowParens").value = "always";
+window.addEventListener('load', () => {
+    'use strict';
+    document.querySelector(
+        '[title="Number of spaces per indentation level."] input'
+    ).value = 4;
+    document.querySelector(
+        '[title="Use single quotes instead of double quotes."] input'
+    ).checked = true;
+    document.querySelector(
+        '[title="Include parentheses around a sole arrow function parameter."] select'
+    ).value =
+        'always';
 });
